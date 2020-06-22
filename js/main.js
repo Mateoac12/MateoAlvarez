871d7cc51b771f010ab968998aa0c16bec578151
Mateo Álvarez <alvarez.fing@gmail.com>
2020-06-22 20:13:36 -0300

    Ultima actualizacion antes de Subirla
    Icono, texto, cambio de color del logo, scroll lento, etc.

---
+++ js/main.js
@@ -0,0 +1,68 @@
+//Smooth Scrolling
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+//ScrollReveal
+
+ScrollReveal().reveal('.navbar', {
+    duration: 2000,
+    distance: '150%',
+    origin: 'top',
+});
+ScrollReveal().reveal('.header-content-left', {
+    delay: 750,
+    duration: 2000,
+    distance: '150%',
+    origin: 'left'
+})
+ScrollReveal().reveal('.header-content-right', {
+    duration: 2000,
+    distance: '150%',
+    origin: 'bottom'
+})
+ScrollReveal().reveal('#proyectos', {
+    duration: 2000,
+    delay: 750
+})
+ScrollReveal().reveal('.project-content-left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '150%'
+})
+ScrollReveal().reveal('.project-content-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '150%'
+})
+ScrollReveal().reveal('#experiencias', {
+    duration: 3000,
+    delay: 750
+})
+ScrollReveal().reveal('.experience-right-content', {
+    duration: 2000,
+    origin: 'right',
+    distance: '150%'
+})
+ScrollReveal().reveal('.experience-left-content', {
+    duration: 2000,
+    origin: 'left',
+    distance: '150%'
+})
+ScrollReveal().reveal('.contacto-content-qr', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '150%'
+})
+ScrollReveal().reveal('#contacto', {
+    duration: 3000,
+    delay: 750
+})
